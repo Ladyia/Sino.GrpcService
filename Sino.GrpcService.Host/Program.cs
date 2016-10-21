4e1836ba3936e3db0cc83ec5df2906b80fefc06e
姚正发 <976691141@qq.com>
2016-10-21 16:16:54 +0800

    完成服务端编写

--- Sino.GrpcService.Host/Program.cs
+++ Sino.GrpcService.Host/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Sino.GrpcService.Impl;
+using System;
+using System.IO;
 
 namespace Sino.GrpcService.Host
 {
@@ -9,6 +9,21 @@ namespace Sino.GrpcService.Host
     {
         public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder();
+
+            var envName = "debug";
+            if (args != null && args.Length > 0)
+                envName = args[0];
+
+            var config = builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json")
+                .AddJsonFile($"appSettings.{envName}.json")
+                .Build();
+
+            RpcConfiguration.Start(config);
+            Console.WriteLine("service start");
+            Console.ReadKey();
+            RpcConfiguration.Stop();
         }
     }
 }
---
+++ Sino.GrpcService.Impl/RpcConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using Sino.GrpcService.Repositories;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Sino.GrpcService.Impl
+{
+    public static class RpcConfiguration
+    {
+        private static Server _server;
+        private static IContainer _container;
+
+        public static void Start(IConfigurationRoot config)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(config).As<IConfigurationRoot>();
+            builder.RegisterInstance(new DataContext(config)).As<IDataContext>();
+            builder.RegisterAssemblyTypes(typeof(IDataContext).GetTypeInfo().Assembly).Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces();
+
+            _container = builder.Build();
+            var servercert = File.ReadAllText(@"G:\\ssl\\server.crt");
+            var serverkey = File.ReadAllText(@"G:\\ssl\\server.key");
+            var keypair = new KeyCertificatePair(servercert, serverkey);
+            var sslCredentials = new SslServerCredentials(new List<KeyCertificatePair>() { keypair });
+            _server = new Server
+            {
+                Services = { MsgService.BindService(new MsgServiceImpl(_container.Resolve<IMsgRepository>())) },
+                Ports = { new ServerPort("0.0.0.0", 9007, sslCredentials) }
+            };
+            _server.Start();
+        }
+
+        public static void Stop()
+        {
+            _server?.ShutdownAsync().Wait();
+        }
+    }
+}
